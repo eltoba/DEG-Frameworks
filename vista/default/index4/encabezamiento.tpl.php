35355eeb3e9739d8b30fa176dca3b062ad1a203d
Mauricio José Tobares <carrozadelamuerte@hotmail.com>
2014-12-23 01:05:21 -0300

    Delete encabezamiento.tpl.php

--- vista/default/index4/encabezamiento.tpl.php
+++
@@ -1,8 +0,0 @@
-<div id="main">
-<!-- HEADER -->
-	<div id="header" class="box">
-		<p id="logo"><a href="#" title="DEG Frameworks"><img width="80" height="40" src="{ruta_plantilla}/design/logo-deg.png" />F<span>rameworks</span></a></p>
-		<h3 id="slogan">Blog, Noticias, Clasificados,<br />
-		Red Social, Foro y mas...</h3>
-	</div>
-<!-- /header -->

1d80b1671cf40be9e4122ed7f4ca3d7287ecff1d
Mauricio José Tobares <carrozadelamuerte@hotmail.com>
2014-12-22 22:00:47 -0300

    Update constantes.php

--- lib/constantes.php
+++ lib/constantes.php
@@ -1,10 +1,5 @@
 <?php
-define('TEMA_INDEX1', 'vista/default/index1/');
-define('TEMA_INDEX2', 'vista/default/index2/');
-define('TEMA_ADMIN', 'vista/default/Adminizio/');
-define('IDIOMA','es_AR');
-define('DB_HOST', 'mysql2.000webhost.com'); // host de tu servidor MySQL
-define('DB_USER', 'a3612886_degforo'); // usuario MySQL
-define('DB_PASS', '3uYUGD2'); // la clave de tu usuario MySQL
-define('DB_NAME', 'a3612886_degforo'); // Nombre de la base de datos
-
+define('DB_HOST', ''); // host de tu servidor MySQL
+define('DB_USER', ''); // usuario MySQL
+define('DB_PASS', ''); // la clave de tu usuario MySQL
+define('DB_NAME', ''); // Nombre de la base de datos
